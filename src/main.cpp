41e750e4d8873536a87d6a6b921f56b6c6781c9a
lukelccse <lukelccse@outlook.com>
2023-08-28 16:51:05 +1000

    image pre processing experimentation

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,50 @@ int main(int argc, char** argv) {
         cout << "Not a valid image file" << endl;
         return -1;
     }
+
+    // experiment Here
+
+    // resize
+    float resize_factor = 1.0f;
+    cv::Mat img_resized;
+    cv::resize(img, img_resized, 
+                cv::Size(img.cols * resize_factor, img.rows * resize_factor),
+                cv::INTER_LINEAR);
+
+    // convert to greyscale image
+    cv::Mat img_grey;
+    cv::cvtColor(img_resized, img_grey, cv::COLOR_BGR2GRAY);
+
+    // blur
+    cv::Mat img_blurred;
+    cv::GaussianBlur(img_grey, img_blurred, cv::Size(5, 5), 0);
+
+    // threashold
+    cv::Mat img_thresh;
+    cv::adaptiveThreshold(img_blurred, img_thresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 27, 0);
+    
+    // mask
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::findContours(img_thresh, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+    cv::Mat drawing = cv::Mat::zeros(img_thresh.size(), CV_8UC3);
+    for(size_t i = 0; i< contours.size(); i++ ){
+        cv::Scalar color = cv::Scalar( 5+i%256, 5+i%256, 5+i%256 );
+        cv::drawContours( drawing, contours, (int)i, color, 2, cv::LINE_8, hierarchy, 0);
+    }
+
+    for(size_t i = 0; i< contours.size(); i++ ){
+        std::cout << hierarchy[i];
+    }
+
+
+
+    // find contours
+
+    // show and stop here.
     
     cv::namedWindow("Simple Demo", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Simple Demo", img);
+    cv::imshow("Simple Demo", drawing);
     
     cv::waitKey(0);
     cv::destroyAllWindows();
